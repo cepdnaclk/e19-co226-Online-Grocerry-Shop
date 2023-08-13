ee9416911f55dbf022768712ebbee13624e374d1
Harnan <115539697+Harnan01@users.noreply.github.com>
2023-08-13 17:28:29 +0530

    LogIn page updated

---
+++ LogIn/login.php
@@ -0,0 +1,31 @@
+<?php
+
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $conn = new mysqli('localhost', 'root', '', 'co226');
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+            $query_user = "SELECT * FROM user WHERE Email = '$email'  and Password = '$password'";
+            $result_user = mysqli_query($conn, $query_user);
+
+            $query_admin = "SELECT * FROM admin WHERE Email = '$email' and Password = '$password'";
+            $result_admin = mysqli_query($conn, $query_admin);
+            
+
+            if (mysqli_num_rows($result_admin) == 1) {
+                session_start();
+                $_SESSION['auth'] = 'true';
+                header("Location: ../Admin/AdminIndex.php");
+
+                exit;
+                } elseif (mysqli_num_rows($result_user) == 1) {
+                    session_start();
+                    $_SESSION['auth'] = 'true';
+                    header("Location: ./html/about.html");
+                    exit;
+                } else {
+                    $output = "Sorry, we can't find an account with this email address. or incorrect password. Please try again";
+                    echo "<script>alert('$output'); window.location.href = './html/LogIn.php';</script>";
+                }
+    ?>
