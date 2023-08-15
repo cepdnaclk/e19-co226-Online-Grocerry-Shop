c78b375c0e79a4c319a7c59158c7c090160209c0
Nithusikan <e19266@eng.pdn.ac.lk>
2023-08-15 20:06:07 +0530

    path updated

--- Admin/AdminIndex.php
+++ Admin/AdminIndex.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
-    
+
 
 
 
@@ -12,15 +13,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 </head>
+
 <body>
     <nav class="navbar bg-body-tertiary" style="background-color:#1877F2 !important;">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-        <img src="images/logo.jpg" alt="Logo is missing" width="80" height="30" style="margin-left: -50px;">
-        </a>
-        <h3 class="text-muted" style="color: white !important;">SkyMart Admin</h3>
-        <button class="btn btn-outline d-flex align-items-center" type="submit" style="border-color: white;color: white; margin-right: -50px;" id="admin_log_out_button">Log Out</button>
-    </div>
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.jpg" alt="Logo is missing" width="80" height="30" style="margin-left: -50px;">
+            </a>
+            <h3 class="text-muted" style="color: white !important;">SkyMart Admin</h3>
+            <button class="btn btn-outline d-flex align-items-center" type="submit" style="border-color: white;color: white; margin-right: -50px;" id="admin_log_out_button">Log Out</button>
+        </div>
     </nav>
 
     <br>
@@ -28,9 +30,9 @@
 
     <nav class="navbar ">
         <div class="container justify-content-center align-item-center" style="background-color: rgba(66, 180, 244, 0.7); height: 180px; width:60%; border-radius: 10mm !important;">
-        <br>
-        <p style="font-size: 23px; font-weight: bold; color: white; margin-bottom: 10px; margin-top: 0.5cm; ">Welcome to the Admin Portal 🌟</p>
-        <p style="text-align: center; font-size: 18px; font-weight: normal; color: white;">As you enter the admin page, your dedication ignites efficiency, your insights kindle innovation, and your actions sculpt excellence. You're the heart of seamless experiences, turning challenges into stepping stones. Your commitment sets the bar high. Welcome to the driving force of progress!</p>
+            <br>
+            <p style="font-size: 23px; font-weight: bold; color: white; margin-bottom: 10px; margin-top: 0.5cm; ">Welcome to the Admin Portal 🌟</p>
+            <p style="text-align: center; font-size: 18px; font-weight: normal; color: white;">As you enter the admin page, your dedication ignites efficiency, your insights kindle innovation, and your actions sculpt excellence. You're the heart of seamless experiences, turning challenges into stepping stones. Your commitment sets the bar high. Welcome to the driving force of progress!</p>
         </div>
     </nav>
 
@@ -51,7 +53,7 @@
                 </div>
             </div>
 
-            <div class="col-4 mx-auto"  >
+            <div class="col-4 mx-auto">
                 <div class="card" style="width: 18rem; margin-left: 60px !important; margin-top: 8px !important; margin-bottom: 8px !important;">
                     <img src="images/product.jpg" class="card-img-top" alt="Product logo is missing" style="height: 200px; width:200px !important; margin-left: 40px !important;">
                     <div class="card-body">
@@ -75,16 +77,23 @@
 
         </div>
     </div>
+
     <script>
-      document.getElementById('admin_log_out_button').addEventListener('click', function() {
-          window.location.href='http://localhost/co226%20project/LogIn/LogIn.html'; // Replace 'Admin_home.html' with your Admin home page URL
-      });
+        // Get a reference to the Log Out button
+        var logOutButton = document.getElementById("admin_log_out_button");
+
+        // Add a click event listener to the button
+        logOutButton.addEventListener("click", function() {
+            // Redirect to the admin.html page
+            window.location.href = "http://localhost/co226/e19-co226-Online-Grocerry-Shop/LogIn/login.php";
+        });
     </script>
 
 
-    
+
     <!--bootstrap-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
